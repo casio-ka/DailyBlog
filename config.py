4bfbb60dcf1f19a4d7584aa1dcb4707c183b76e9
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-30 12:18:08 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
-
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wainainakasyoka:W41n41n4@localhost/dailyblog'
+    POSTS_PER_PAGE = 3
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/img'
@@ -25,8 +26,10 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
-
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wainainakasyoka:W41n41n4@localhost/dailyblog_test'
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wainainakasyoka:W41n41n4@localhost/dailyblog'
     DEBUG = True
 
 config_options = {
