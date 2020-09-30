079850fbb76ee1f378a25e263a1bf3af46b7a791
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-30 12:18:17 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role
+from app.models import User,Role, BlogPost, Comments,Subscribers
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -21,7 +21,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Role=Role )
+    return dict(app = app,db = db,User = User, Role=Role, BlogPost=BlogPost, Comments=Comments, Subscribers=Subscribers )
 
 if __name__ == '__main__':
     manager.run()
