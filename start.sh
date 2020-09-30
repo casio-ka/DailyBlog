f4196275eda0056373298637af782f196798111a
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-30 16:18:54 +0300

    Update start.sh
    Added quotes API_URL

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export MAIL_USERNAME=testingemailpk6@gmail.com
 export MAIL_PASSWORD=moringatest96
 export SECRET_KEY='eskimo'
+export API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
 
 python3.8 manage.py server
